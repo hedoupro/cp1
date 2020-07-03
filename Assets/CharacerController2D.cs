e67ef87e79af928738a92e37cf3e83c639072473
hedoupro <67784691+hedoupro@users.noreply.github.com>
2020-07-03 11:17:33 +0100

    Add files via upload

---
+++ Assets/CharacerController2D.cs
@@ -0,0 +1,73 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacerController2D : MonoBehaviour
+{
+    public float speed;
+    public float smooth;
+    Rigidbody2D rb2d;
+    Animator animator;
+    public float jumpForce;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+
+        animator = GetComponent<Animator>();
+    }
+
+    void FixedUpdate()
+    {
+        
+       
+        float x = Input.GetAxisRaw("Horizontal") * speed;
+       
+     
+        rb2d.velocity = new Vector2(x,rb2d.velocity.y);
+        animator.SetBool("Running", x!=0); 
+
+     animator.SetBool("Jumping", !grounded);
+    
+        if (x > 0 && !facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+        else if (x< 0 && facingRight)
+        {
+            // ... flip the player.
+            Flip();
+        }
+    }
+bool grounded;
+public Collider2D groundCheck;
+public LayerMask groundLayer;
+private void Update()
+    {
+        
+        // Is the player touching the ground ?
+        grounded = groundCheck.IsTouchingLayers(groundLayer);
+        Debug.Log(grounded);
+				
+		// Only jump if the player is grounded and has pressed the Space bar
+				
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+            rb2d.AddForce(new Vector2(0f, jumpForce));
+				
+    }
+ bool facingRight = true;
+void Flip()
+    {
+        
+        facingRight = !facingRight;
+
+        
+        Vector2 scale = transform.localScale;
+
+        scale.x *= -1;
+
+        transform.localScale = scale;
+        animator.SetBool("Jumping", !grounded); 
+    }
+}
---
+++ Assets/HealthManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthManager : MonoBehaviour
+{
+  const float MAXHEALTH = 100f;
+  float health;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+    void Die()
+    {
+	    GetComponent<CharacerController2D>().enabled = false;
+    }
+    public void TakeDamage(float amount){
+      health -= amount;
+      if(health <= 0){
+        health = 0;
+        Die();
+      }
+    }
+}
+
+
+
+
+
